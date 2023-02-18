40483878d249a29fd34a5a76302a3a4e03020d47
David Wentsch <david.wentsch@edu.fhdw.de>
2023-02-18 23:15:49 +0100

    Wetter anzeige zeigt die richtige Temperatur an Neues Design Typing hinzugefügt

--- app.js
+++ app.js
@@ -8,11 +8,14 @@ class Chatbox {
         this.args = {
             openButton: document.querySelector('.chatbox__button'),
             chatBox: document.querySelector('.chatbox__support'),
-            sendButton: document.querySelector('.send__button')
+            sendButton: document.querySelector('.send__button'),
+
+
 
         }
 
         this.state = false;
+        this.aktive = true;
         this.messages = [];
     }
 
@@ -23,6 +26,7 @@ class Chatbox {
         openButton.addEventListener('click', () => this.toggleState(chatBox))
 
         sendButton.addEventListener('click', () => this.onSendButton(chatBox))
+
 
         const node = chatBox.querySelector('input');
         node.addEventListener("keyup", ({key}) => {
@@ -35,12 +39,26 @@ class Chatbox {
     toggleState(chatbox) {
         this.state = !this.state;
 
+
         // show or hides the box
         if (this.state) {
             chatbox.classList.add('chatbox--active')
+            if(this.aktive) {
+
+
+                setTimeout(() => {
+   let msg = {name: 'bot_uttered', message: 'Hallo'};
+                this.messages.push(msg);
+                this.updateChatText(chatbox);
+
+                }, 3000)
+                this.aktive = false;
+            }
+
 
         } else {
             chatbox.classList.remove('chatbox--active')
+
         }
     }
 
@@ -52,41 +70,75 @@ class Chatbox {
 
         const textField = chatbox.querySelector('input');
         const text1 = textField.value;
+
         if (!text1) return;
 
-        const msg1 = {name: 'user_uttered', message: text1};
+        let msg1 = {name: 'user_uttered', message: text1};
         ws.send(text1);
         this.messages.push(msg1);
 
+
         const handleMessage = e => {
-            console.log(e)
-            const msg2 = {name: 'bot_uttered', message: e.data};
-            this.messages.push(msg2);
-            this.updateChatText(chatbox);
-            textField.value = '';
+        let msg2={};
 
             if (e.data === 'Das Wetter in Stuttgart ist:') {
+
                 const weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=2825297&appid=e3b561757bafc55f9075e613caf26f7b';
                 fetch(weatherUrl, {method: 'GET', headers: {}})
                     .then(res => res.json())
                     .then(data => {
-                        const output = this.getFormatedOutput(data);
+                        const output = this.getFormatedTemp(data);
 
-                        const msg3 = {name: 'bot_uttered', message: output};
-                        this.messages.push(msg3);
+              msg2 = {name: 'bot_uttered', message: output};
+                        this.messages.push(msg2);
                         this.updateChatText(chatbox);
+                         textField.value = '';
+
+
                     })
                     .catch(error => console.error('Error', error));
+
+
+
+            }else{
+
+
+             console.log(e)
+             msg2 = {name: 'bot_uttered', message: e.data};
+
+
+            this.messages.push(msg2);
+            this.updateChatText(chatbox);
+            textField.value = '';
+
             }
+
+
             ws.removeEventListener('message', handleMessage);
         };
+
         ws.addEventListener('message', handleMessage);
+
+
+
+
     }
 
-    getFormatedOutput(data) {
-        return `${data.list[data.list.length - 1].weather[0].description}
-                        Temperatur: ${Math.round(data.list[data.list.length - 1].main.temp) / 100}°C
-                        Gefühlt wie: ${Math.round(data.list[data.list.length - 1].main.feels_like) / 100}°C\n`;
+    getFormatedTemp(data) {
+        console.log([data])
+        return `In Stuttgart sind es ${Math.round((data.list[0].main.temp)-273.15)}°C
+                        Gefühlt wie: ${Math.round((data.list[0].main.feels_like)-273.15)}°C\n`;
+    }
+
+
+
+
+        getFormatedWeather(data) {
+        console.log([data])
+        return `In Stuttgart sind es: ${data.list[0].weather[0].description}
+        
+                        Temperatur: ${Math.round((data.list[0].main.temp)-273.15)}°C
+                        Gefühlt wie: ${Math.round((data.list[0].main.feels_like)-273.15)}°C\n`;
     }
 
     updateChatText(chatbox) {
